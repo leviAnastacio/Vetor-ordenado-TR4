c62949801257c08729bb2736cccb3c31f2c00c01
leviAnastacio <120414088+leviAnastacio@users.noreply.github.com>
2022-12-13 13:44:27 -0300

    Update main_vet.c

--- main_vet.c
+++ main_vet.c
@@ -2,46 +2,49 @@
 #include <stdlib.h>
 #include "ordvetor.h"
 
-//Função de comparação para os elementos desejados no teste
+//Função de comparação dos elementos desejados para o teste
 int myCOMP(void* x, void* y){
 	int ptx = *((int*)x);
 	int pty = *((int*)y);
-	if(ptx - pty < 5){
+	if(ptx < pty){
 		return -1;
 	}else{
-		if(ptx - pty > 5) 
+		if(ptx > pty) 
 			return 1;
 		else 
 			return 0;
 	}
 }
 
-//Função de imprimir os elementos do vetor
+//Função de imprimir os elementos-teste do vetor
 void print(void** vet, int P){
+	int i;
 	printf("VETOR DE %d TAMANHO:\n", P);
-	for(int i = 0; i < P; i++){
+	for(i = 0; i < 10; i++){
 		printf(" %d ", *((int*)vet[i]));
 	}
 	printf("\n");
 }
 
 int main(){
-   	VETORORD* vet = VETORD_create(10, myCOMP);//cria um vetor de 10 posições com base na função de comparação para testes.
+   	VETORORD* vet = VETORD_create(10, myCOMP);//Define um ponteiro que recebe um vetor de 10 posições referente à função de comparação dos testes
+   	
    	int* x;
+   	int i;
    	
    	printf("INCLUINDO... \n");
-	for(int i = 0; i < 10; i++){
+	for(i = 0; i < 10; i++){//Laço que permite a adição dos 10 elementos gerados no vetor
 		x = malloc(sizeof(int));
 		*x = i*10;
-		VETORD_add(vet, x);//inclusão dos elementos gerados no vetor.
+		VETORD_add(vet, x);//Chamada de função que adiciona os valores dentro do vetor
 	}
 	
-	print(vet->elems, vet->P);//imprime os elementos adicionados.
+	print(vet->elems, vet->P);//Imprime o valor de cada posição do vetor.
   
   	printf("REMOVENDO!");
-  	VETORD_remove(vet);//remoção do primeiro elemento do vetor.
+  	VETORD_remove(vet);//Chamada de função que remove o elemento da primeira posição do vetor.
   	
-	print(vet->elems, vet->P);//imprime o vetor após a exclusão do primeiro elemento.
+	print(vet->elems, vet->P);//imprime os elementos das posições do vetor após a remoção do primeiro item.
    
 	return 0;  
 }
